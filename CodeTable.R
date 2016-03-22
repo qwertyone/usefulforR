f5e243c698629cb1200b2943b3cb3cc9c720c441
qwertyone <qwertyone@users.noreply.github.com>
2016-03-22 15:15:13 +0100

    Update CodeTable.R

--- CodeTable.R
+++ CodeTable.R
@@ -1,6 +1,6 @@
-##RQDA Code Table
-x<-getCodeTable()
+##RQDA Code Key Table
+x<-getCodingTable()
 x<-x[c(2,4)]
 x<-x[order(x[1]),]
-tableCode<- x[match(unique(x$cid), x$cid),]
-write.table(tableCode,file="ProjectCodeKey.txt",sep="|",col.names=TRUE)
+tableCode<- x[match(unique(x$codename), x$codename),]
+write.table(tableCode,file="ProjectCodeKey.txt",sep="|",col.names=TRUE,row.names=FALSE)
